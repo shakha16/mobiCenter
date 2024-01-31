ca0c977baaf1397b485200318a03bf4f47446f9c
Shoxrux <ganiorzikulov@gmail.com>
2024-01-31 17:04:03 +0500

    d

--- vite-project/src/main.jsx
+++ vite-project/src/main.jsx
@@ -8,7 +8,7 @@ import { BrowserRouter } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename='/mobiCenter'>
+    <BrowserRouter>
       <App />
     </BrowserRouter>
   </React.StrictMode>,
