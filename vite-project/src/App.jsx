a8963519a1e8b07a6cd1489418b6b856a26d064b
Shoxrux <ganiorzikulov@gmail.com>
2024-01-31 17:15:15 +0500

    lox

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,6 +1,4 @@
 import React from 'react';
-import { Route, Routes } from 'react-router-dom'
-import Layout from './Layout/Layout'
 import Home from './pages/Home'
 import Tovar from './pages/Tovar'
 
@@ -8,14 +6,7 @@ import Tovar from './pages/Tovar'
 
 function App() {
     return (
-        <Routes>
-            <Route path='/' element={<Layout />}>
-                <Route index element={
-                    <Home/>
-                } />
-                <Route path='/tovar' element={<Tovar />} />
-            </Route>
-        </Routes>
+        <Home />
     )
 }
 
